257c6d1be65d0927983d54bd01e247e5150081d5
borkovip <philipp.borkovic@edu.htl-villach.at>
2025-05-26 10:06:20 +0200

    Add user, role, category, and venue entities, modules, and services

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToMany,
+} from 'typeorm';
+import { Event } from '../../events/entities/event.entity';
+import { Role } from '../../roles/entities/role.entity';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ name: 'first_name', type: 'varchar' })
+  firstName: string;
+
+  @Column({ name: 'last_name', type: 'varchar' })
+  lastName: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  updatedAt: Date;
+
+  @OneToMany(() => Event, (event) => event.organizer)
+  organizedEvents: Event[];
+
+  @ManyToMany(() => Role, (role) => role.users)
+  roles: Role[];
+}
