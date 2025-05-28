bd1fafd5eeb5062cbfd855ddd33db39744ff6238
borkovip <philipp.borkovic@edu.htl-villach.at>
2025-05-28 09:25:41 +0200

    feat: add category and venue entities, modules, and role type enum for role management

---
+++ backend/src/auth/enums/role-type.enum.ts
@@ -0,0 +1,13 @@
+/**
+ * Enum defining the available roles in the application.
+ * Used for role-based access control.
+ * 
+ * @enum RoleType
+ * @author Philipp Borkovic
+ */
+export enum RoleType {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+  ORGANIZER = 'ORGANIZER',
+  ATTENDEE = 'ATTENDEE',
+} 
